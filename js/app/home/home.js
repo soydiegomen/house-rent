7e0f8d79d75e55725306255a75bb24591da49f53
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-10-17 21:59:36 -0500

    Agregue que en la pantalla del listado de casas se muestre la información de las casas que trae el servicio.

--- js/app/home/home.js
+++ js/app/home/home.js
@@ -6,16 +6,43 @@
 	HomeCtrl.$inject = ['homeService'];
 
 	/**@ngInject*/
-	function HomeCtrl(, homeService){
+	function HomeCtrl(homeService){
 		var homeCtrl = this;
+
+		//Properties
+		homeCtrl.listHouses = null;
+
+		//Atributtes
 
 		//Initialize controller
 		activate();
 
 		function activate(){
 			console.log('Activated HomeCtrl');
+			getHouses();
+		}
+
+		//Events
+
+		//Helpers
+		function getHouses(){
 			homeService.getHouses().then(function(data){
-				console.log('getHouses', data);
+				setupHouseList(data);
+				homeCtrl.listHouses = data;
+			});
+		}
+
+		function setupHouseList(listHouses){
+			angular.forEach(listHouses, function(value, key){
+				if(value.filesData.length > 0){
+					var defautltImage = value.filesData[0];
+					defautltImage.completeUrl = 'http://housefiles.loc:8888/' + defautltImage.fileUrl;
+					//Add to House JSON the default image with complete url
+					value.defautltImage = defautltImage;
+				}else{
+					//Set default image for house without image
+					value.defautltImage = { completeUrl : 'img/demo/houses/casa-dos.jpg'};
+				}
 			});
 		}
 	}
